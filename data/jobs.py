89fee6e8e24159bc3515a6dad439d57ad094ae3c
Daniil <danyvd@yandex.ru>
2022-04-05 17:59:34 +0300

    - Добавлена первичная структура простого сайта с базой данных, создан README.md

---
+++ data/jobs.py
@@ -0,0 +1,11 @@
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+from sqlalchemy_serializer import SerializerMixin
+
+
+class Jobs(SqlAlchemyBase, SerializerMixin):
+    __tablename__ = 'jobs'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.Text, nullable=True)
